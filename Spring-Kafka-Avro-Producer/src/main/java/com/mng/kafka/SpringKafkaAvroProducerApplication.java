05807650bb97aa90229f664c18130e863f756f98
Mekala.Nagendra <Mekala.Nagendra@WKSBAN29RBW1060.corp.mphasis.com>
2019-05-23 19:42:13 +0530

    apache kafka avro spring boot consumer

---
+++ Spring-Kafka-Avro-Producer/src/main/java/com/mng/kafka/SpringKafkaAvroProducerApplication.java
@@ -0,0 +1,12 @@
+package com.mng.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaAvroProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaAvroProducerApplication.class, args);
+	}
+}
---
+++ Spring-Kafka-Avro-Producer/src/test/java/com/mng/kafka/SpringKafkaAvroProducer/SpringKafkaAvroProducerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mng.kafka.SpringKafkaAvroProducer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringKafkaAvroProducerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
